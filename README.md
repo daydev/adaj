85f0f5b42cc5c504621aa6bda24518f41f7a9a48
Egor Day <edaydev@gmail.com>
2013-04-10 22:54:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Primitive utility for asynchonous JSON request with Dart.
+Four main HTTP methods (GET, POST, PUT and DELETE) are supported, as well, as simple success/error callbacks
---
+++ lib/adaj.dart
@@ -0,0 +1,45 @@
+library adaj;
+
+import "dart:html";
+import "dart:json" as json;
+
+void ajax(String url, {String method: "GET", Map data: null, Function success: null, Function error: null}) {
+
+  HttpRequest request = new HttpRequest();
+
+  request.open(method, url);
+  if (success != null) {
+    request.onLoad.listen((event) {
+      if ((request.status - 200) < 100) {
+        print(request.status);
+        success(request.responseText);
+      } else if (error != null) {
+        error();
+      }
+    });
+  }
+
+  if (data != null) {
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(json.stringify(data));
+  } else {
+    request.send();
+  }
+
+}
+
+void get(String url, Function success) {
+  ajax(url, success: success);
+}
+
+void post(String url, Map data, [Function success]) {
+  ajax(url, method: "POST", data: data, success: success);
+}
+
+void put(String url, Map data, [Function success]) {
+  ajax(url, method: "PUT", data: data, success: success);
+}
+
+void delete(String url, [Function success]) {
+  ajax(url, method: "DELETE", success: success);
+}
